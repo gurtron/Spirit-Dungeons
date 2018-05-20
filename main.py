8dcae7495d50597d535133bd553fe4813f7666a5
gurtron <Garrett.Commons@dpi.wi.gov>
2018-05-20 00:18:12 -0500

    latest files

--- main.py
+++ main.py
@@ -1,12 +1,56 @@
 import discord
 import random
 
+# List of invented data structures
+#
+#
+#
+#
 
-class character():
-    name = ""
+class item:
+    itemName = ""
+    itemDescription = ""
+    itemEquip = False
+
+
+class inventory:
+    items = {}
+
+    def checkItem(self, itemType):
+        return itemType in self.items
+
+    def checkCount(self, itemType):
+        if self.checkItem(itemType):
+            return self.items[itemType]
+        else:
+            return 0
+
+    def addItem(self, itemToAdd, itemNumber):
+        if self.checkItem(itemToAdd):
+            self.items[itemToAdd] += itemNumber
+        else:
+            self.items[itemToAdd] = itemNumber
+
+    def removeItem(self, itemToRemove, itemNumber):
+        if self.checkItem(itemToRemove) >= itemNumber:
+            self.items[itemToRemove] -= itemNumber
+        else:
+            return False
+
+
+  #  def countItems(self, items):
+
+  #  def returnItems(self, items):
+
+
+
+
+
+class character(inventory):
+    charName = ""
     maxSpirit = 1
     spirit = 1
-    inventory = []
+    charInventory = inventory()
     location = ""
     emblem = ""
     charType = ""
@@ -22,8 +66,6 @@ class town():
 
 class players(character):
     equipment = []
-    materials = []
-    party = ""
 
 
 class enemies(character):
@@ -31,11 +73,25 @@ class enemies(character):
     combatAbilities = []
 
 
-class menus():
-    textPrompt = []
-    reactions = []
-    parentMenu = None
-    childMenus = []
+class menus:
+    client = None
+    messageDictionary = {}
+
+    def __init__(self, client):
+        self.client = client
+
+    def addMessageToList(self,message,reactions):
+        self.messageDictionary[message] = reactions
+
+    @client.event
+    async def on_message(self, message):
+        if message.content.startswith("😉"):
+            replyString = ' '.join(list(self.messageDictionary.keys())), " \n\n", self.messageDictionary
+            botMessage = await self.client.send_message(message.channel,replyString)
+            for message in self.messageDictionary:
+                reactionList = self.messageDictionary[message]
+                for emoji in reactionList:
+                    await self.client.add_reaction(botMessage, emoji)
 
 
 class dungeonInfo():
@@ -43,60 +99,3 @@ class dungeonInfo():
     numRooms = random.randint(4,8)
     themes = []
     materialsTable = []
-
-
-def prw(botprint):
-    client.send_message(message.channel, botprint)
-
-
-TOKEN = 'NDQ1MDgzOTgxODg1NDcyNzc4.DdlmZA.7R8n1IbPUvV4smtJQXhdCsEaJko'
-
-client = discord.Client()
-
-
-@client.event
-#async def newplayer():
-#    newplayerinfo = players()
-#
-#    prw('''As you walk, the forest grows denser.  What little light shines through the canopy is muted by the dull grey bark on the gnarled trees.  The cold clings to your skin and gives you pause.  The stench of wet moss emanates from above.  You follow a blue glowing circuit embedded in the rocks at your feet.  At times you lose it in the bushes and have to wade through thorns to keep on your path.  Where this circuit leads, you do not know.
-#
-#You have followed the circuit for so long, you lost track of time.  Has it been hours or days?  As you consider giving up and turning back, you come upon a boulder with a figure sitting on it.  The figure appears to be a statue, but has subtle, organic movements.  Its shoulders appear relaxed, and its feet sway to and fro.  Its eye stalks track you, scanning you up and down.
-#
-#"You, too, follow the circuit? And what is your name?"''')
-
-#    newplayerinfo.name = await
-
-
-async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('!hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
-        await client.send_message(message.channel, msg)
-
-    if message.content.startswith('🖐'):
-        emojis = ['👍', '👎']
-        auth = message.author
-        msg = await client.send_message(message.channel, 'Choose a reaction.')
-        for emoji in emojis:
-            await client.add_reaction(msg, emoji)
-        res = await client.wait_for_reaction(['👍', '👎'], user=auth, message=msg)
-        await client.send_message(message.channel, '{0.user} reacted with {0.reaction.emoji}!'.format(res))
-
-    if message.content.startswith('*⃣'):
-        emojis = ['📖', 'ℹ']
-        msg = await client.send_message(message.channel, 'You have accessed the menu.  Choose an option.')
-        for option in emojis:
-            await client.add_reaction(msg, option)
-
-
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
-
-
-client.run(TOKEN)
